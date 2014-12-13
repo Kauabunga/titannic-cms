d8068995bc92573a79e828a52570a642bd20c9ae
Carson Bruce <carson.bruce@solnet.co.nz>
2014-12-13 22:04:17 +1300

    Added all seeds

--- server/config/seed.js
+++ server/config/seed.js
@@ -70,6 +70,101 @@ var schemas = [
         googleDocContentId: '0B84YdCmz0nrQck5TWG9TZC1jTGs'
       }
     ]
+  },
+  {
+    schema: {
+      name : 'Capabilities schema',
+      info : 'Capabilities schema',
+      googleDocSchemaId: '0B84YdCmz0nrQRFMzQjhjMy01YVU'
+    },
+    documents: [
+      {
+        name : 'Capabilities dev content',
+        info : 'Capabilities dev json content',
+        googleDocContentId: '0B84YdCmz0nrQOHhnOEJsLWY1akE'
+      },
+      {
+        name : 'Capabilities live content',
+        info : 'Capabilities live json content',
+        googleDocContentId: '0B84YdCmz0nrQb093WGY3ZjZWbVk'
+      }
+    ]
+  },
+  {
+    schema: {
+      name : 'Clients schema',
+      info : 'Clients schema',
+      googleDocSchemaId: '0B84YdCmz0nrQR0tuaVNFX1NDVUU'
+    },
+    documents: [
+      {
+        name : 'Clients dev content',
+        info : 'Clients dev json content',
+        googleDocContentId: '0B84YdCmz0nrQOHhnOEJsLWY1akE'
+      },
+      {
+        name : 'Clients live content',
+        info : 'Clients live json content',
+        googleDocContentId: '0B84YdCmz0nrQaWQ3dzRWdjhkanc'
+      }
+    ]
+  },
+  {
+    schema: {
+      name : 'Case studies schema',
+      info : 'Case studies schema',
+      googleDocSchemaId: '0B84YdCmz0nrQcmI1cXl3eFZ3SGs'
+    },
+    documents: [
+      {
+        name : 'Case studies dev content',
+        info : 'Case studies dev json content',
+        googleDocContentId: '0B84YdCmz0nrQbFExTXpXVjZlLW8'
+      },
+      {
+        name : 'Case studies live content',
+        info : 'Case studies live json content',
+        googleDocContentId: '0B84YdCmz0nrQbndockJGNDQ1WGM'
+      }
+    ]
+  },
+  {
+    schema: {
+      name : 'Contact schema',
+      info : 'Contact schema',
+      googleDocSchemaId: '0B84YdCmz0nrQTnV1dTNVMGhmaWs'
+    },
+    documents: [
+      {
+        name : 'Contact dev content',
+        info : 'Contact dev json content',
+        googleDocContentId: '0B84YdCmz0nrQZi1DSmdoYURWTms'
+      },
+      {
+        name : 'Contact live content',
+        info : 'Contact live json content',
+        googleDocContentId: '0B84YdCmz0nrQZTRMUkZJeXFXbHM'
+      }
+    ]
+  },
+  {
+    schema: {
+      name : 'Quotes schema',
+      info : 'Quotes schema',
+      googleDocSchemaId: '0B84YdCmz0nrQY2lOcDNkVjYwN00'
+    },
+    documents: [
+      {
+        name : 'Quotes dev content',
+        info : 'Quotes dev json content',
+        googleDocContentId: '0B84YdCmz0nrQYTNGN0MyOW9MM1E'
+      },
+      {
+        name : 'Quotes live content',
+        info : 'Quotes live json content',
+        googleDocContentId: '0B84YdCmz0nrQWExzV2VKemh1Um8'
+      }
+    ]
   }
 
 ];
