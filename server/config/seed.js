52d29b72e2cde0cefb0bf8d411908e6a561d6952
Carson Bruce <carson.bruce@solnet.co.nz>
2014-12-13 20:34:57 +1300

    Mongo db seeding

--- server/config/seed.js
+++ server/config/seed.js
@@ -5,31 +5,76 @@
 
 'use strict';
 
-//var Thing = require('../api/thing/thing.model');
+
+var q = require('q');
 var User = require('../api/user/user.model');
+var Document = require('../api/document/document.model');
+var Schema = require('../api/schema/schema.model');
 
-//Thing.find({}).remove(function() {
-//  Thing.create({
-//    name : 'Development Tools',
-//    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-//  }, {
-//    name : 'Server and Client integration',
-//    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-//  }, {
-//    name : 'Smart Build System',
-//    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-//  },  {
-//    name : 'Modular Structure',
-//    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-//  },  {
-//    name : 'Optimized Build',
-//    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-//  },{
-//    name : 'Deployment Ready',
-//    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-//  });
-//});
 
+
+var schemas = [
+  {
+    schema: {
+      name : 'Nav dev schema',
+      info : 'Nav dev json schema',
+      googleDocSchemaId: '0B84YdCmz0nrQWjc5SzdDVEYwWXM'
+    },
+    documents: [
+      {
+        name : 'Nav dev content',
+        info : 'Nav dev json content',
+        googleDocContentId: '0B84YdCmz0nrQamtJTUJUVlpBOU0'
+      },
+      {
+        name : 'Nav live content',
+        info : 'Nav live json content',
+        googleDocContentId: '0B84YdCmz0nrQZUtVODBYNkJFTVE'
+      }
+    ]
+  }
+
+];
+
+
+
+
+/**
+ *
+ */
+Schema.find({}).remove(function() {
+  Document.find({}).remove(function() {
+
+    var i;
+
+    for(i = 0; i < schemas.length; i++){
+      var currentSchema = schemas[i];
+
+      Schema.create(currentSchema.schema, function(event, schemaMongo){
+
+        console.log('seeded schema ' + currentSchema.schema.name);
+
+        var j;
+        for(j = 0; j < currentSchema.documents.length; j++){
+          var currentDocument = currentSchema.documents[j];
+          currentDocument.schemaId = schemaMongo._id;
+
+          Document.create(currentDocument, function(){
+            console.log('seeded document ' + currentDocument.name);
+          });
+        }
+
+      });
+
+    }
+
+  });
+
+});
+
+/**
+ *
+ */
 User.find({}).remove(function() {
   User.create({
     provider: 'local',
