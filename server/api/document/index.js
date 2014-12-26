84cb222a9dc20921a89e6577f96ff306eb4d5bb3
Carson Bruce <carson.bruce@solnet.co.nz>
2014-12-26 22:07:11 +1300

    admin role api permissions

--- server/api/document/index.js
+++ server/api/document/index.js
@@ -8,10 +8,10 @@ var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.index);
 router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', controller.create);
+router.post('/', auth.hasRole('admin'), controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
 router.patch('/:id', auth.isAuthenticated(), controller.update);
-router.delete('/:id', auth.isAuthenticated(), controller.destroy);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/preview/:id', controller.getPreview);
 
 module.exports = router;
